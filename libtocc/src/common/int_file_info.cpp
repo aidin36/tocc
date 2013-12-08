c12029f54d1d5f1453a1fda64deaf589ceaa678c
aidin <aidin@aidinlap.(none)>
2013-12-08 06:19:08 +0330

    Issue #5 FileInfoCollection implemented.

--- libtocc/src/common/int_file_info.cpp
+++ libtocc/src/common/int_file_info.cpp
@@ -26,6 +26,11 @@ namespace libtocc
     this->id = file_id;
   }
 
+  std::string IntFileInfo::get_id()
+  {
+    return this->id;
+  }
+
   void IntFileInfo::set_tags(std::vector<std::string> new_tags)
   {
     this->tags = new_tags;
@@ -61,4 +66,61 @@ namespace libtocc
     return this->traditional_path;
   }
 
+  FileInfoCollection::FileInfoCollection()
+  {
+  }
+
+  FileInfoCollection::FileInfoCollection(std::vector<IntFileInfo> file_infos)
+  {
+    this->file_infos = file_infos;
+  }
+
+  FileInfoCollection::FileInfoCollection(IntFileInfo file_infos[], int count)
+  {
+    // Fastest way for appending an array to a vector.
+    this->file_infos.reserve(count);
+    this->file_infos.insert(this->file_infos.end(),
+                            &file_infos[0],
+                            &file_infos[count - 1]);
+  }
+
+  void FileInfoCollection::add_file_info(IntFileInfo file_info)
+  {
+    this->file_infos.push_back(file_info);
+  }
+
+  void FileInfoCollection::add_file_infos(std::vector<IntFileInfo> file_infos)
+  {
+    // Fastest way for appending two vectors.
+    this->file_infos.insert(this->file_infos.end(),
+                            file_infos.begin(),
+                            file_infos.end());
+  }
+
+  void FileInfoCollection::add_file_infos(IntFileInfo file_infos[], int count)
+  {
+    this->file_infos.reserve(this->file_infos.size() + count);
+    this->file_infos.insert(this->file_infos.end(),
+                            &file_infos[0],
+                            &file_infos[count - 1]);
+  }
+  
+  std::vector<IntFileInfo> FileInfoCollection::get_file_infos()
+  {
+    return this->file_infos;
+  }
+
+  std::vector<std::string> FileInfoCollection::get_file_ids()
+  {
+    std::vector<std::string> result;
+    result.reserve(this->file_infos.size());
+
+    for (std::vector<IntFileInfo>::iterator iterator = this->file_infos.begin();
+         iterator != this->file_infos.end(); iterator++)
+    {
+      result.push_back((*iterator).get_id());
+    }
+
+    return result;
+  }
 }
