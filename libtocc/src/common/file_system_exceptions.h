aa67b5ce955f8b6fd39c3a3ec62b9659804dcbbe
Aidin Gharibnavaz <aidin@aidinhut.com>
2014-04-22 05:30:25 +0430

    In the comment at files's beginning, Tocc was written in capital letters.

--- libtocc/src/common/file_system_exceptions.h
+++ libtocc/src/common/file_system_exceptions.h
@@ -1,19 +1,19 @@
 /*
- * This file is part of TOCC. (see <http://www.github.com/aidin36/tocc>)
+ * This file is part of Tocc. (see <http://www.github.com/aidin36/tocc>)
  * Copyright (C) 2013, 2014, Aidin Gharibnavaz <tocc@aidinhut.com>
  *
- * TOCC is free software: you can redistribute it and/or modify
+ * Tocc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TOCC is distributed in the hope that it will be useful,
+ * Tocc is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- *  along with TOCC.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Tocc.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LIBTOCC_FILE_SYSTEM_EXCEPTIONS_H_INCLUDED
